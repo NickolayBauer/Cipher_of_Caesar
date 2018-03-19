c2aaffc9ca44a61fa28cf11d6ff52cc35fd1a8f0
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-03-19 19:58:34 +0300

    Update cipher_of_caesar.py

--- cipher_of_caesar.py
+++ cipher_of_caesar.py
@@ -1,2 +1,16 @@
-# you're code here
-print('test for trevis')
+list_alpha = []
+list_word = []
+alpa = "абвгдеёжзийклмнопрстуфхшщыэюя"
+
+for i in alpa:
+    list_alpha.append(i)
+
+word = input("слово: ")
+key = int(input("ключ: "))
+new = ''
+for i in word:
+    if i == ' ':
+        new += i
+    else:
+        new += list_alpha[(list_alpha.index(i)) - key]
+print(new)
